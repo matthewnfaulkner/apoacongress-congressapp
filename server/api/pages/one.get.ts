e9ee3ee3074c2caba4963550213f2aa5e1a26302
Matthew Faulkner <matthewnfaulkner@gmail.com>
2026-03-16 16:35:33 +0800

    changed pricing card from single button to button group

--- server/api/pages/one.get.ts
+++ server/api/pages/one.get.ts
@@ -476,7 +476,8 @@ export default defineEventHandler(async (event) => {
 		}
 
 		return page;
-	} catch {
+	} catch (error) {
+		console.log(error)
 		throw createError({ statusCode: 500, statusMessage: 'Page not found' });
 	}
 });
