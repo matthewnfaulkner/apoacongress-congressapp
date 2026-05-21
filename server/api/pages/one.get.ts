40a4ccb60a4bca5bfa72a73649877091afb01d23
Matthew Faulkner <matthewnfaulkner@gmail.com>
2026-05-21 15:58:20 +0800

    reconfigtured charge table block

--- server/api/pages/one.get.ts
+++ server/api/pages/one.get.ts
@@ -489,7 +489,6 @@ export default cachedEventHandler(async (event) => {
     return `pages-${permalink}`
   },
   shouldBypassCache: (event) => {
-	return true;
     const config = useRuntimeConfig()
     return config.public.isSandbox || getQuery(event).preview === 'true'
   },
